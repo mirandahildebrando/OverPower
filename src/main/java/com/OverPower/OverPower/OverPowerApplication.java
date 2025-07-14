b29503c17d931d5a1ee2d20c87c96458eda87f16
mirandahildebrando <mirandahildebrando5@gmail.com>
2025-07-14 20:20:13 -0300

    first commit

---
+++ src/main/java/com/OverPower/OverPower/OverPowerApplication.java
@@ -0,0 +1,13 @@
+package com.OverPower.OverPower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverPowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverPowerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/OverPower/OverPower/OverPowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OverPower.OverPower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverPowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
